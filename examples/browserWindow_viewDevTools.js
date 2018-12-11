7fadd7c83f82868e0856407b144709619de907bc
xiaogang <wxungang@163.com>
2018-12-11 20:11:39 +0800

    init：setDevToolsWebContents tests!

--- examples/browserWindow_viewDevTools.js
+++ examples/browserWindow_viewDevTools.js
@@ -19,6 +19,31 @@ function _initDevTools() {
 
 // Open the DevTools.
   view.webContents.openDevTools({mode: 'detach'});
+
+  view.webContents.on('did-finish-load', (e) => {
+    view.webContents.enableDeviceEmulation({
+      screenPosition: 'mobile',
+      //屏幕 尺寸 & 同步 exports.render.screenSize
+      screenSize: {
+        width: 375,
+        height: 667
+      },
+      //渲染 区域
+      viewSize: {
+        width: 375,
+        height: 667
+      },
+      //起始点
+      viewPosition: {
+        x: 0,
+        y: 0
+      },
+      //dpr :2
+      deviceScaleFactor: 2,
+      //缩放
+      scale: 1
+    })
+  });
 }
 
 
